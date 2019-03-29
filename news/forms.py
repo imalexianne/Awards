621411e18ddced78bad44d11d6d7efddfb4acd80
imalexianne <imanirax@yahoo.fr>
2019-03-29 08:30:02 +0200

    first commit

---
+++ news/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Image,Profile,Comments
+
+class ProfileForm(forms.ModelForm):
+  
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class ImageForm(forms.ModelForm):
+    
+    class Meta:
+        model = Image
+        exclude = ['user','profile']
+
+class CommentsForm(forms.ModelForm):
+
+    class Meta:
+        model = Comments
+        exclude = ['user']
+
